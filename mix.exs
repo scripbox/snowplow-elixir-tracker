9846b08f19704cc703d4a34a623df5f48f7ec80a
Nitin Misra <nitin.misra23@gmail.com>
2018-12-27 16:14:16 +0530

    Accept HTTPoison options from configuration

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SnowplowTracker.MixProject do
   def project do
     [
       app: :snowplow_tracker,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
--- test/support/request_mock.ex
+++ test/support/request_mock.ex
@@ -1,7 +1,7 @@
 defmodule SnowplowTracker.RequestMock do
   require HTTPoison
 
-  def get(_, _) do
+  def get(_url, _headers, _options) do
     send(self(), "request sent")
     {:ok, %HTTPoison.Response{status_code: 200, body: "response"}}
   end
