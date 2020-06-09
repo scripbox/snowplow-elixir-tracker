2a4b431ad5355cb8685326988905399a6d9fcc14
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2020-06-09 16:58:22 +1000

    Enable server in test

--- lib/application.ex
+++ lib/application.ex
@@ -2,14 +2,9 @@ defmodule SnowplowTracker.Application do
   use Application
 
   def start(_type, _args) do
-    children =
-      if Mix.env() != :test do
-        [
-          {SnowplowTracker.Emitters.Server, []}
-        ]
-      else
-        []
-      end
+    children = [
+      {SnowplowTracker.Emitters.Server, []}
+    ]
 
     Supervisor.start_link(children, strategy: :one_for_all)
   end
